b78fec09ee606e56bdfeb7affd81f4ce6f1cb05c
Jaza K <jazakhan2246@gmail.com>
2020-06-04 19:54:56 -0600

    create responsive background

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.sass';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <svg viewBox='0 0 1024 768' preserveAspectRatio='none'>
+        <path d='M0,750 C650,730 500,154 1024,154 L1024,0 0,0 0,750' fill='#28262C' />
+      </svg>
     </div>
   );
 }
