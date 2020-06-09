1aae437cc21377155212147b371a4283e5451eee
Jaza K <jazakhan2246@gmail.com>
2020-06-08 23:09:59 -0600

    create each page as its own separate component

---
+++ src/Sources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Sources() {
+    return (
+        <div className="Sources">
+            <h1>Sources page</h1>
+            <svg viewBox='0 0 1024 768' preserveAspectRatio='none'>
+                <path d='M0,750 C650,730 500,154 1024,154 L1024,0 0,0 0,750' fill='#1f1d22' />
+            </svg>
+        </div>
+    );
+}
+
+export default Sources;
---
+++ src/WhatToDo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function WhatToDo() {
+    return (
+        <div className="WhatToDo">
+            <h1>What to do page</h1>
+        </div>
+    );
+}
+
+export default WhatToDo;
