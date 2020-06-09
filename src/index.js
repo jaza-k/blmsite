83549ca643a353d20e54f13b6aedfd13695e31d3
Jaza K <jazakhan2246@gmail.com>
2020-06-08 23:04:52 -0600

    implement routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './Home';
+import About from './About';
+import WhatToDo from './WhatToDo';
+import Sources from './Sources';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/home" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/whattodo" component={WhatToDo} />
+        <Route path="/sources" component={Sources} />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
