5e86f5b8571a3a26689b95f30cd58c8c092b57f1
Jaza K <jazakhan2246@gmail.com>
2020-06-10 19:20:55 -0600

    background/logo redesign

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import './Home.sass';
-import NavBar from './NavBar'
+import fist from './fist.png'
 
 function Home() {
   return (
     <div className="Home">
-      <NavBar />
-      <div className="text-body">
-        <h1 className="opening">In the U.S., African Americans are 2.5 more likely to be killed by police than white people.</h1>
-        <h4 className="secondary">Thousands more around the world get subjected to other types of racial discriminaton each day.</h4>
-        <p className="learn-more">Learn what you can do to help</p>
+      <div class="container">
+        <div className="split-left">
+          <div className="logo">
+            <img className="fist" src={fist} alt="fist" />
+            <h1 className="blacklives-text">BlackLives<span className="matter-text">Matter</span></h1>
+          </div>
+        </div>
+        <section class="split-right">
+
+        </section>
       </div>
-      <svg viewBox='0 0 1024 768' preserveAspectRatio='none'>
-        <path d='M0,750 C650,730 500,154 1024,154 L1024,0 0,0 0,750' fill='#1f1d22' />
-      </svg>
     </div>
   );
 }
