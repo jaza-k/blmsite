c2257e5257e4a6a043b823db46fa97606775502f
Jaza K <jazakhan2246@gmail.com>
2020-06-10 20:38:24 -0600

    update homepage content

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Home.sass';
 import fist from './fist.png'
+import protestfists from './protest.svg'
 
 function Home() {
   return (
@@ -11,9 +12,16 @@ function Home() {
             <img className="fist" src={fist} alt="fist" />
             <h1 className="blacklives-text">BlackLives<span className="matter-text">Matter</span></h1>
           </div>
+          <div className="text-body">
+            <h2 className="opening-text">In the U.S., African Americans are 2.5 times <span className="bold-text">more likely to be killed</span> by police than white people.</h2>
+            <p className="secondary-text">Thousands more around the globe get subjected to racial discrimination each day.</p>
+          </div>
+          <div className="learn-more-btn">
+            <span className="learn-more-text">Learn what you can do to help</span>
+          </div>
+          <img className="protest-fists" src={protestfists} alt="protestfists"></img>
         </div>
         <section class="split-right">
-
         </section>
       </div>
     </div>
