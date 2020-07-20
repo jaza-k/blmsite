c9f04800449e17ac50a211939fed5a34e8380c5d
Jaza K <jazak@iMac.local>
2020-07-20 01:02:50 -0600

    add functionality to render cards

--- src/components/WhatToDo.js
+++ src/components/WhatToDo.js
@@ -1,82 +1,88 @@
 import React from 'react';
 import '../styles/Webpage.sass';
 import Main from '../components/Main';
+import Card from '../components/Card';
 
-const WhatToDo = () => (
-    <Main>
-        <div className="WhatToDo">
-            <div className="wrapper">
-                <h1 className="main-heading">What To Do</h1>
-                <div className="information-body">
-                    <p className="information-text">Here's a list of ways you can help right now:</p>
+const WhatToDo = () => {
 
-                    <div className="section-wrapper">
-                        <div className="section-box">
-                            <h3 className="section-heading">Organizations to donate to</h3>
-                            <ul className="section-list">
-                                <li className="list-item"><a href="https://ca.gofundme.com/f/georgefloyd" target="_blank" rel=" noopener noreferrer">George Floyd Memorial Fund</a></li>
-                                <li className="list-item"><a href="https://www.joincampaignzero.org/" target="_blank" rel="noopener noreferrer">Campaign Zero</a></li>
-                                <li className="list-item"><a href="https://www.innocenceproject.org/" target="_blank" rel="noopener noreferrer">Innocence Project</a></li>
-                                <li className="list-item"><a href="https://justiceforbreonna.org/" target="_blank" rel="noopener noreferrer">Justice For Breonna</a></li>
-                                <li className="list-item"><a href="https://www.blackvisionsmn.org/" target="_blank" rel="noopener noreferrer">Black Visions Collective</a></li>
-                                <li className="list-item"><a href="https://www.reclaimtheblock.org/" target="_blank" rel="noopener noreferrer">Reclaim The Block</a></li>
-                                <li className="list-item"><a href="https://www.nationalbailout.org/" target="_blank" rel="noopener noreferrer">National Bail Out</a></li>
-                            </ul>
-                        </div>
+    const renderCards = () => {
+        const petitions = [
+            { name: "Justice For George Floyd", url: "https://www.amnesty.org/en/get-involved/take-action/george-floyd-police-violence-usa/" },
+            { name: "Justice For Breonna Taylor", url: "https://www.change.org/p/andy-beshear-justice-for-breonna-taylor?source_location=topic_page" },
+            { name: "Justice For Ahmaud Arbery", url: "https://www.runwithmaud.com/#petition" },
+            { name: "Color Of Change", url: "https://act.colorofchange.org/sign/heroesactpetition-congress" },
+            { name: "Hands Up Act", url: "https://www.change.org/p/us-senate-hands-up-act" },
+            { name: "Justice For Tony McDade", url: "https://actionnetwork.org/letters/demand-justice-for-tony-mcdade?source=direct_link&" },
+            { name: "Cut Ties With The FOP", url: "https://act.colorofchange.org/sign/cut-ties-fop" }
+        ]
+        const organizations = [
+            { name: "George Floyd Memorial Fund", url: "https://ca.gofundme.com/f/georgefloyd" },
+            { name: "Campaign Zero", url: "https://www.joincampaignzero.org/" },
+            { name: "Innocence Project", url: "https://www.innocenceproject.org/" },
+            { name: "Justice For Breonna", url: "https://justiceforbreonna.org/" },
+            { name: "Black Visions Collective", url: "https://www.blackvisionsmn.org/" },
+            { name: "Reclaim The Block", url: "https://www.reclaimtheblock.org/" },
+            { name: "National Bail Out", url: "https://www.nationalbailout.org/" }
+        ]
 
-                        <div className="section-box">
-                            <h3 className="section-heading">Petitions to sign</h3>
-                            <ul className="section-list">
-                                <li className="list-item">Justice For George Floyd</li>
-                                <li className="list-item">Colors Of Change</li>
-                                <li className="list-item">Justice For Ahmaud Arbery</li>
-                                <li className="list-item">Colors Of Change</li>
-                                <li className="list-item">Justice For Ahmaud Arbery</li>
-                                <li className="list-item">Colors Of Change</li>
-                                <li className="list-item">Justice For Ahmaud Arbery</li>
-                            </ul>
-                        </div>
+        return (
+            <div className="section-wrapper">
+                <Card heading="Organizations to donate to">
+                    <ul className="section-list">
+                        {organizations.map((organization, index) => <li key={index} className="list-item">{organization.name}</li>)}
+                    </ul>
+                </Card>
 
-                        <div className="section-box">
+                <Card heading="Petitions to sign">
+                    <ul className="section-list">
+                        {petitions.map((petition, index) => <li key={index} className="list-item">{petition.name}</li>)}
+                    </ul>
+                </Card> 
+
+                <Card heading="Text or call">
+                    <h1>Yo</h1>
+                </Card>
+
+                <Card heading="Register to vote">
+                    <h1>Yo</h1>
+                </Card>
+
+                <Card heading="Educate yourself">
+                    <h1>Yo</h1>
+                </Card>
+            </div>
+        )
+    }
+
+    return (
+        <Main>
+            <div className="WhatToDo">
+                <div className="wrapper">
+                    <h1 className="main-heading">What To Do</h1>
+                    <div className="information-body">
+                        <p className="information-text">Here's a list of ways you can help right now:</p>
+
+                        {renderCards()}
+
+                        {/* <div className="section-box">
                             <h3 className="section-heading">Text or call</h3>
                             <ul className="section-list">
-                                <li className="list-item">Text "JUSTICE" to 668366</li>
-                                <li className="list-item">Text "FLOYD" to 55156</li>
-                                <li className="list-item">Get in contact with the District Attorney & Minneapolis Mayor.</li>
-                                <li className="list-item">Text "FLOYD" to 55156</li>
-                                <li className="list-item">Get in contact with the District Attorney & Minneapolis Mayor.</li>
-                                <li className="list-item">Text "FLOYD" to 55156</li>
-                                <li className="list-item">Get in contact with the District Attorney & Minneapolis Mayor.</li>
+                                <li className="list-item" id="textorcall">Text "JUSTICE" to 668366</li>
+                                <li className="list-item" id="textorcall">Text "FLOYD" to 55156</li>
+                                <li className="list-item" className="contact-heading">Get in contact with officials:</li>
+                                <div className="contact">
+                                    <li className="list-item" id="textorcall">Minneapolis District Attorney</li>
+                                    <li className="list-item" id="textorcall">Mike Freeman</li>
+                                    <li className="list-item" id="textorcall">(612)-348-5550</li>
+                                    <li className="list-item" id="textorcall">citizeninfo@hennepin.us</li>
+                                </div>
                             </ul>
-                        </div>
-
-                        <div className="section-box">
-                            <h3 className="section-heading">Register to vote</h3>
-                            <ul className="section-list">
-                                <li className="list-item">Text "JUSTICE" to 668366</li>
-                                <li className="list-item">Text "FLOYD" to 55156</li>
-                                <li className="list-item">Get in contact with the District Attorney & Minneapolis Mayor.</li>
-                                <li className="list-item">Text "FLOYD" to 55156</li>
-                                <li className="list-item">Get in contact with the District Attorney & Minneapolis Mayor.</li>
-                                <li className="list-item">Text "FLOYD" to 55156</li>
-                                <li className="list-item">Get in contact with the District Attorney & Minneapolis Mayor.</li>
-                            </ul>
-                        </div>
-
-                        <div className="section-box">
-                            <h3 className="section-heading">Text or call</h3>
-                            <ul className="section-list">
-                                <li className="list-item">Text "JUSTICE" to 668366</li>
-                                <li className="list-item">Text "FLOYD" to 55156</li>
-                                <li className="list-item">Get in contact with the District Attorney & Minneapolis Mayor.</li>
-                            </ul>
-                        </div>
+                        </div> */}
                     </div>
-
                 </div>
             </div>
-        </div>
-    </Main>
-);
+        </Main>
+    )
+}
 
 export default WhatToDo;
