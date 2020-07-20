a2c78f9a34d10d4ce01e59025ccf3836c8652e01
Jaza K <jazak@iMac.local>
2020-07-20 01:02:11 -0600

    fix formatting

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,26 +12,27 @@ const Main = ({children}) => {
         const hiddenDiv = document.querySelector('.hidden-div');
         const nav = document.querySelector('.nav-links');
         const navLinks = document.querySelectorAll('.nav-links li');
-            // show menu
-            hiddenDiv.classList.toggle('toggle-hidden-div');
-            // toggle nav menu
-            nav.classList.toggle('nav-active');
+
+        // show menu
+        hiddenDiv.classList.toggle('toggle-hidden-div');
+        // toggle nav menu
+        nav.classList.toggle('nav-active');
     
-            // animate links
-            navLinks.forEach((link, index) => {
-                if (link.style.animation) {
-                    link.style.animation = '';
-                } else {
-                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-                }
-            });
-            // burger animation
-            burger.classList.toggle('toggle-menu');
+        // animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+        // burger animation
+        burger.classList.toggle('toggle-menu');
     }
 
     return (
         <div className="Main">
-            <div class="container">
+            <div className="container">
                 <div className="split-left">
                     <div className="hidden-div">
                         <ul className="nav-links">
@@ -83,7 +84,7 @@ const Main = ({children}) => {
                         </nav>
                     </div>
                 </div>
-                <section class="split-right"></section>
+                <section className="split-right"></section>
             </div>
         </div>
     );
