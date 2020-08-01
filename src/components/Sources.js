5e2008e6d2d5b7d662d067035aa650756811f07e
Jaza K <jazak@iMac.local>
2020-07-31 20:41:53 -0600

    add content/styling for sources page

--- src/components/Sources.js
+++ src/components/Sources.js
@@ -2,18 +2,36 @@ import React from 'react';
 import '../styles/Webpage.sass';
 import Main from '../components/Main';
 
-const Sources = () => (
-    <Main>
-        <div className="Sources">
-            <div className="wrapper">
-                <h1 className="main-heading">Sources</h1>
-                <div className="information-body">
-                    <p className="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    <p className="about-text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident</p>
+const Sources = () => {
+
+    const renderSites = () => {
+        const websites = [
+            { name: "BLM - Canada - All Black Lives Matter", url: "https://blacklivesmatter.ca/" },
+            { name: "What We Believe - Black Lives Matter", url: "https://blacklivesmatter.com/what-we-believe/" },
+            { name: "Black Lives Matter: From Hashtag to Movement", url: "https://www.adl.org/education/educator-resources/lesson-plans/black-lives-matter-from-hashtag-to-movement" },
+            { name: "Statistics and Data - Black Lives Matter: Policing and Incarceration", url: "https://libguides.princeton.edu/c.php?g=598338&p=4142165" },
+            { name: "Black Lives Matter - international activist movement", url: "https://www.britannica.com/topic/Black-Lives-Matter" },
+        ]
+        return (
+            <ul className="site-list">
+                {websites.map((site, index) => <li key={index} className="list-item" id="site"><a href={site.url} target="_blank" rel="noopener noreferrer">{site.name}</a></li>)}
+            </ul>
+        )
+    }
+
+    return (
+        <Main>
+            <div className="Sources">
+                <div className="wrapper">
+                    <h1 className="main-heading">Sources</h1>
+                    <div className="information-body">
+                        <p className="information-text">Check out these websites for further information:</p>
+                        {renderSites()}
+                    </div>
                 </div>
             </div>
-        </div>
-    </Main>
-);
+        </Main>
+    )
+}
 
 export default Sources;
