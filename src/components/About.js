f21f7ebad611fed58a90c0244cc4bb85bb781e8a
Jaza K <jazak@iMac.local>
2020-07-31 19:42:40 -0600

    minor content modification

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ const About = () => (
                 <div class="information-body">
                     <p className="information-text">Racial injustice has existed for centuries, and is unfortunately still a prominent issue around the world today.</p>
                     <p className="information-text">Following the death of George Floyd, Ahmaud Arbery, Breonna Taylor, and many more, millions of people around the globe have participated in protests and demonstrations against racism and police violence.</p>
-                    <p className="information-text">This website was created as a means of spreading awareness around this fight for racial equality. The <a href="/actions" style={{textDecoration: 'underline', color: '#341F2C'}}>What To Do</a> page contains various resources and ways you can help in supporting this cause and the <a href="/sources" style={{textDecoration: 'underline', color: '#341F2C'}}>Sources</a> page contains additional resources.</p>
+                    <p className="information-text">This website was created as a means of spreading awareness around this fight for racial equality. The <a href="/actions" style={{textDecoration: 'underline', color: '#341F2C'}}>What To Do</a> page contains various ways you can help in supporting this cause and the <a href="/sources" style={{textDecoration: 'underline', color: '#341F2C'}}>Sources</a> page contains additional resources.</p>
                 </div>
             </div>
         </div>
